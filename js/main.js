9e5fdf4b0bb62eb838a0ecb14fbc559bedfcb8a6
Nastya2510 <s.laurent@mail.ru>
2024-06-20 16:43:13 +0300

    Сбор данных с карточки. работа с радиокнопками

--- js/main.js
+++ js/main.js
@@ -7,12 +7,13 @@ btnNext.forEach(function (button) {
     button.addEventListener("click", function () {
         // this ссылается на ту кнопку, по которой кликнули
         var thisCard = this.closest("[data-card]")
-        var thisCardNumber = parseInt(thisCard.dataset.card)
-        var nextCard = thisCardNumber + 1
 
-        // Добавление и удаление класса hidden
-        thisCard.classList.add("hidden")
-        document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden")
+
+        if (thisCard.dataset.validate == "novalidate") {
+            navigate("next", thisCard)
+        } else {
+            navigate("next", thisCard)
+        }
     })
 })
 
@@ -21,11 +22,63 @@ btnPrev.forEach(function (button) {
     button.addEventListener("click", function () {
         // this ссылается на ту кнопку, по которой кликнули
         var thisCard = this.closest("[data-card]")
-        var thisCardNumber = parseInt(thisCard.dataset.card)
-        var nextCard = thisCardNumber - 1
-
-        // Добавление и удаление класса hidden
-        thisCard.classList.add("hidden")
-        document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden")
+        navigate("prev", thisCard)
     })
 })
+
+function navigate(direction, thisCard) {
+    var thisCardNumber = parseInt(thisCard.dataset.card)
+    var nextCard
+
+    if (direction == "next") {
+        nextCard = thisCardNumber + 1
+    } else if (direction == "prev") {
+        nextCard = thisCardNumber - 1
+    }
+
+    // Добавление и удаление класса hidden
+    thisCard.classList.add("hidden")
+    document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden")
+}
+
+// объект с ответами на все карточки
+var answers = {
+    2: null,
+    3: null,
+    4: null,
+    5: null
+
+}
+
+// сбор данных с текущей карточки
+function gatherCardData(number) {
+    var question
+    var result = []
+
+    // находим карточку по номеру и data-атрибуту
+    var currentCard = document.querySelector(`[data-card = "${number}"]`)
+
+    // находим главный вопрос карточки
+    question = currentCard.querySelector("[data-question]").innerHTML
+
+    // найдем все заполненные значения
+    // радиокнопки в пределах текущей карточки
+    var radioValues = currentCard.querySelectorAll('[type="radio"]')
+    radioValues.forEach(function (item) {
+        // сбор значений радиокнопок
+        if (item.checked) {
+            result.push({
+                name: item.name,
+                value: item.value
+            })
+        }
+    })
+
+    var data = {
+        question: question,
+        answer: result
+    }
+
+    return data
+
+}
